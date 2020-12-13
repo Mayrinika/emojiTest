21b4520c293887ab3da0c24970c4fd40258ab83c
Mayrinika <mashuliy_u@mail.ru>
2020-12-13 23:30:30 +0500

    :(

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import EmojiWidget from 'emojiwidget';
+
+function onExtensionClick(tab) {
+    const inputList = document.querySelectorAll('input');
+    for(const input of inputList) {
+        new EmojiWidget(input);
+    }
+}
+
+chrome.browserAction.onClicked.addListener(onExtensionClick);
